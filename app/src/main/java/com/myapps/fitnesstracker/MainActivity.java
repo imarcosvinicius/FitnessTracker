f98a9b6306d92b8e5197dcf44e9516c7c4ea3ef1
Marcos Vinicius Nunes <imarcosvinicius@outlook.com>
2022-05-28 13:28:45 -0300

    Initial commit

---
+++ app/src/main/java/com/myapps/fitnesstracker/MainActivity.java
@@ -0,0 +1,29 @@
+package com.myapps.fitnesstracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View btnImc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnImc = findViewById(R.id.btn_imc);
+
+        btnImc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ImcActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
